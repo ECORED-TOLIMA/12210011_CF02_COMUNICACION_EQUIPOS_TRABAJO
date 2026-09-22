14298aad51519f0f908da88b38f9502ed4dad335
Francisco Vásquez <francisjvasquez23@gmail.com>
2026-09-22 16:41:15 -0500

    Avance
    Avance

--- src/config/global.js
+++ src/config/global.js
@@ -182,7 +182,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/12210011_CF02_CFA.pdf',
       },
       {
         icono: 'fas fa-download',
